49db6b444ee8f8e31c20318b4a6abe94916967d5
Your Name <you@example.com>
2018-05-04 14:15:05 +0300

    cmon

---
+++ index.js
@@ -0,0 +1,19 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+
+class Home extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Home />,
+  
+)
